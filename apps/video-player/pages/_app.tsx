583ced47031f36bb43ea9867f69af922db3891c8
Marian Zburlea <marian@wowdop.com>
2023-04-27 20:31:41 +0100

    feat: add bare bone nextjs app as video-player

---
+++ apps/video-player/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+
+const CustomApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Welcome to video-player!</title>
+      </Head>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  );
+};
+
+export default CustomApp;
---
+++ apps/video-player/pages/index.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+  return (
+    <div>
+      <h1>Laning page for video</h1>
+      <p>Change me later</p>
+    </div>
+  );
+};
+
+export default LandingPage;
